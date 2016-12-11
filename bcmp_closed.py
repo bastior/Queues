1f81135a1dcf427e811afa0d548ea9000e52a01e
woyski <jonak.t.o@gmail.com>
2016-12-11 20:16:28 +0100

    iz of workings!

--- bcmp_closed.py
+++ bcmp_closed.py
@@ -50,17 +50,32 @@ class Node(object):
 
 class BcmpNetworkClosed(object):
     def __init__(self, R, N, k, mi_matrix, p, node_info, epsilon):
+        """
+        :param R: classes amount
+        :param N: nodes amount
+        :param k: list of request in system per class
+        :param mi_matrix: matrix[node,class]
+        :param p: list of matrices, probabilities for each class
+        :param node_info: list of tuples (type, servers amount)
+        :param epsilon: stop condition
+        """
         self.R = R
         self.N = N
         self.k = k
         self.k_sum = sum(k)
         self.mi_matrix = mi_matrix
+        # store servers amount per node (needed for ro recalculations)
+        # im too lazy to clean that up
         self.m = list(map(lambda x: x[1], node_info))
-        self.e = self._calculate_visit_ratios(p)
-        self._lambdas = [0.00001 for _ in range(R)]
-        self._get_call_chains(node_info)
-        self.calculate_ro(self)
         self.epsilon = epsilon
+        # raw probabilites have to be converted
+        self.e = self._calculate_visit_ratios(p)
+        # Initate lambdas with zeros
+        self._lambdas = [0.00001 for _ in range(R)]
+        # determine function types and store closures for each
+        self._get_call_chains(node_info)
+        # calculate ro values pre first iteration
+        self.calculate_ro()
 
     @staticmethod
     def _calculate_visit_ratios(p):
@@ -71,41 +86,61 @@ class BcmpNetworkClosed(object):
         return -1 * np.hstack(visit_ratios)
 
     def calculate_ro(self):
+        """
+            Ro calculation without explicit lambda matrix, based on
+            lambda_ir = e_ir * lambda_r
+        :return:
+        """
         ro_matrix = [[(self._lambdas[r] * self.e[i, r]) / (self.m[i] * self.mi_matrix[i, r])
-                      for r in range(R)] for i in range(N)]
+                                for r in range(R)] for i in range(N)]
+        ro_matrix = np.matrix(ro_matrix)
         self.ro = ro_matrix.sum(1)
 
     def _get_call_chains(self, node_info):
+        """
+        Populates call matrix with closures, a bit different
+        way to determine computations based on node type.
+        Implies heavy usage of class state.
+        :param node_info: list of tuples from init
+        :return: none
+        """
         if any(map(lambda tup: tup[1] == 0, node_info)):
             raise ValueError('0 amount of servers is not allowed')
         if any(map(lambda (type, m): type !=1 and m != 1, node_info)):
             raise ValueError('Only type 1 can have more than one server')
 
-        self.call_chain_matrix = [[] for _ in range(N)]
-        for i in range(self.N):
-            for r in range(self.R):
-                self.call_chain_matrix[i].append(
+        self.call_chain_matrix = [[] for _ in range(R)]
+        for r in range(self.R):
+            for i in range(self.N):
+                self.call_chain_matrix[r].append(
                     self.get_fix_iteration(r, i, node_info[i]))
 
     def get_fix_iteration(self, r, i, node_info):
+        """
+        Lets hope all equations are valid. No guarantees given for now.
+        :param r: class index
+        :param i: node index
+        :param node_info: tuple(type, servers amount)
+        :return: function calculating fix for (type, node, class) scheme
+        """
         type_, m = node_info
 
         def type1():
-            nom = self.e[i][r] / self.mi_matrix[i][r]
-            denom = (1 - ((self.k_sum - 1) / self.k)*self.ro_list[i])
+            nom = self.e[i,r] / self.mi_matrix[i,r]
+            denom = (1 - ((self.k_sum - 1) / self.k_sum) * self.ro[i]).item()
             return nom / denom
 
         def type2():
-            sum1 = self.e[i][r] / self.mi_matrix[i][r]
+            sum1 = self.e[i, r] / self.mi_matrix[i, r]
             mul1 = (sum1 / m) / (1 - (self.k_sum-m-1)*self.ro[i]/self.k_sum-m)
             mul2 = ((m * self.ro[i])**m) / (math.factorial(m) * (1 - self.ro[i]))
             mul31 = sum([((m*self.ro[i])**k)/math.factorial(k) for k in range(m - 1)])
             mul32 = ((m * self.ro[i])**m) / math.factorial(m) * (1-1/(1-self.ro[i]))
 
-            return sum1 + mul1 * mul2 / (mul31 + mul32)
+            return (sum1 + mul1 * mul2 / (mul31 + mul32)).item()
 
         def type3():
-            return self.e[i][r] / self.mi_matrix[i][r]
+            return self.e[i, r] / self.mi_matrix[i, r]
 
         if m == 1 and type_ in frozenset([1,2,4]):
             return type1
@@ -114,15 +149,22 @@ class BcmpNetworkClosed(object):
         elif type_ == 3:
             return type3
 
+        raise RuntimeError("Unsupported (type, amount) pair (%s, %s) " % node_info)
+
     def iterate(self):
         error = self.epsilon + 1
         while error > self.epsilon:
             old_lambdas = list(self._lambdas)
-            for r in enumerate(self._lambdas):
-                self._lambdas[r] = self.k[r] / sum(map(lambda x: x(), self.call_chain_matrix[i]))
-                err = sum([(self._lambdas[rr] - old_lambdas[rr])**2 for rr in enumerate(self._lambdas)])
-                error = math.sqrt(err)
-                self.calculate_ro()
+            for r in range(R):
+                self._lambdas[r] = self.k[r] / sum(map(lambda x: x(), self.call_chain_matrix[r]))
+
+            err = sum([(self._lambdas[r] - old_lambdas[r])**2 for r in range(R)])
+            error = math.sqrt(err)
+            self.calculate_ro()
+
+    def get_measures(self):
+        pass
+
 
 if __name__ == '__main__':
     # Classes amount
@@ -150,37 +192,24 @@ if __name__ == '__main__':
     # single class transition probabilities
     classes = [p1, p2]
 
-    # step 1 - compute visit ratios for closed network
-    visit_ratios = []
-    for cl in classes:
-        from pprint import pprint
-        A = cl.T - np.diagflat([1, 1, 1])
-        # pprint(A)
-        visit_ratios.append(null(A))
-        # pprint(visit_ratio)
-        # pprint(A*visit_ratio)
-    # TODO why those bloody coeffs are negative
-    visit_ratios = -1 * np.hstack(visit_ratios)
-
-    # TODO make fixed parameters to fully test that
     # generate network parameters
     # node types
-    types = [random.randint(1, 4) for _ in range(N)]
+    types = [1,1,3]
     # servers amount in respect to node type
-    m = [random.randint(1, 4) if types[i] == 1 else 1 for i in range(len(types))]
+    m = [2,1,1]
     # amount of request by class
-    K = [random.randint(1, 4) for _ in range(R)]
+    K = [3,3,3]
 
-    # initialize lambdas
-    lambdas = np.array([0.00001 for _ in range(R)])
+    # Raise solver
+    solver = BcmpNetworkClosed(
+        R=R,
+        N=N,
+        k=K,
+        mi_matrix=mi,
+        p=classes,
+        node_info=zip(types,m),
+        epsilon=0.0001
+    )
 
-    # k summed
-    k_sum = sum(K)
-
-    # fixed point iterations
-    epsilon = 0
-    e = epsilon + 1
-    while e > epsilon:
-        ro_matrix, roi_list = get_ro_components(lambdas, visit_ratios, m, mi)
-
+    solver.iterate()
 
